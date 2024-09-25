ea4cf92249c41526ae5536539ad9e661e18a7924
muyulong <mylyao@qq.com>
2024-09-25 13:19:26 +0800

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
   const githubName = import.meta.env.VITE_GITHUB_NAME;
   const homeUrl = import.meta.env.VITE_HOME_URL;
   const emailUrl = import.meta.env.VITE_EMAIL_URL;
-  const siteIcp = import.meta.env.VITE_SITE_ICP;
+  // const siteIcp = import.meta.env.VITE_SITE_ICP;
 
   return (
     <footer id="footer">
@@ -33,7 +33,7 @@ const Footer = () => {
           <CustomLink to="https://uptimerobot.com/" text="UptimeRobot" />
           &nbsp;接口&nbsp;|&nbsp;检测频率 5 分钟
         </p>
-        <p>
+{/*         <p>
           Copyright&nbsp;&copy;&nbsp;2020&nbsp;-&nbsp;{new Date().getFullYear()}
           &nbsp;
           <CustomLink to="https://www.imsyy.top/" text="無名" />
@@ -43,7 +43,7 @@ const Footer = () => {
               <CustomLink to="https://beian.miit.gov.cn/" text={siteIcp} />
             </React.Fragment>
           ) : null}
-        </p>
+        </p> */}
       </div>
     </footer>
   );
